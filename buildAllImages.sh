6250e87562693f70ebcf5f036afc310fe197ffb4
talenddev <leo@talenddev.com>
2014-11-19 22:40:13 +0000

    added scripts

---
+++ buildAllImages.sh
@@ -0,0 +1,7 @@
+docker build -t talenddev/baseimage baseimage
+docker build -t talenddev/python python
+docker build -t talenddev/couchpotato couchpotato
+docker build -t talenddev/transmission transmission
+docker build -t talenddev/sickbeard sickbeard
+docker build -t talenddev/headphones headphones
+docker build -t talenddev/xdm xdm
