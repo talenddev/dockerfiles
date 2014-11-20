5d1a2344cf40cf2b1c8db35690cdaaffd5af7f15
talenddev <leo@talenddev.com>
2014-11-20 07:24:50 +0000

    Update start.sh

--- couchpotato/start.sh
+++ couchpotato/start.sh
@@ -2,6 +2,3 @@
 umask 000
 
 exec /sbin/setuser share python /opt/CouchPotatoServer/CouchPotato.py --data_dir /data --daemon
-
-# Did this because if couchpotato restarts, it kills the container.
-# tail -f /data/logs/CouchPotato.log
