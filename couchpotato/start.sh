717e70563acb0cc162b58279d11af593a6ca68fd
talenddev <leo@talenddev.com>
2015-03-06 19:25:45 +0000

    Update start.sh

--- couchpotato/start.sh
+++ couchpotato/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 umask 000
 
-exec /sbin/setuser share python /opt/CouchPotatoServer/CouchPotato.py --data_dir /data --daemon
+exec /sbin/setuser share python /opt/CouchPotatoServer/CouchPotato.py --data_dir /data
